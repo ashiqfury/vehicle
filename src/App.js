4c4c53e85a38e4688d21fb52ea99a5cfe198e0c7
Fury <ashiqasraf07@gmail.com>
2022-01-09 17:38:27 +0530

    Changed to hash router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import Topbar from './components/Topbar'
 import VehicleMaster from './components/VehicleMaster'
 import VendorMaster from './components/VendorMaster'
